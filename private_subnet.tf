dc242a0571708abd65108366059693c0fa3a1277
Atlas Kerr <atlaskerr@gmail.com>
2017-03-18 20:54:23 -0500

    fixed availability zone bug that counted by characters instead of by the azs variable

--- private_subnet.tf
+++ private_subnet.tf
@@ -1,21 +1,21 @@
 resource "aws_subnet" "private" {
-	count = "${ var.private ? length(var.azs) : 0 }"
+	count = "${ var.private ? length(split(",", var.azs)) : 0 }"
 
 	vpc_id = "${ var.vpc_id }"
 	cidr_block = "${ cidrsubnet(var.vpc_cidr, 8, var.starting_bit_private + count.index) }"
-	availability_zone = "${ element(split(",", var.azs, count.index) }"
+	availability_zone = "${ element(split(",", var.azs), count.index) }"
 
 	lifecycle {
 		create_before_destroy = true
 	}
 
 	tags {
-		Name = "${ var.name }-private-subnet-${ element(spilt(",", var.azs), count.index) }"
+		Name = "${ var.name }-private-subnet-${ element(split(",", var.azs), count.index) }"
 	}
 }
 
 resource "aws_route_table" "private" {
-	count = "${ var.private ? 1 : 0 }"
+	count = "${ var.private ? length(split(",", var.azs)) : 0 }"
 
 	vpc_id = "${ var.vpc_id }"
 
@@ -25,15 +25,15 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route" "nat" {
-	count = "${ var.private ? length(var.azs) : 0 }"
+	count = "${ var.private ? length(split(",", var.azs)) : 0 }"
 
-	route_table_id			= "${ aws_route_table.private.id }"
+	route_table_id			= "${ element(aws_route_table.private.*.id, count.index) }"
 	destination_cidr_block	= "0.0.0.0/0"
-	nat_gateway_id			= "${ var.nat_id }"
+	nat_gateway_id			= "${ var.public ? element(aws_nat_gateway.nat.*.id, count.index) : var.nat_id }"
 }
 
 resource "aws_route_table_association" "private" {
-	count = "${ var.private ? length(var.azs) : 0 }"
+	count = "${ var.private ? length(split(",", var.azs)) : 0 }"
 
 	subnet_id = "${ element(aws_subnet.private.*.id, count.index) }"
 	route_table_id = "${ element(aws_route_table.private.*.id, count.index) }"

bcbe4bd7cffeb5ac23027a9945c7d0d7a3414dfe
Atlas Kerr <atlaskerr@gmail.com>
2017-03-18 20:51:15 -0500

    added variables

---
+++ variables.tf
@@ -0,0 +1,56 @@
+variable "name" {
+	type = "string"
+	default = "default"
+}
+
+variable "azs" {
+	type = "string"
+	default = "us-east-1a,us-east-1b,us-east-1c"
+}
+
+variable "cost_center" {
+	type = "string"
+	description = "An optional tag for company cost allocation."
+	default = "none"
+}
+
+variable "vpc_id" {
+	type = "string"
+	description = "The ID of the VPC that the subnet(s) will be located in."
+}
+
+variable "vpc_cidr" {
+	type = "string"
+	description = "The CIDR of the VPC that the subnet(s) will be located in."
+}
+
+variable "public" {
+	description = "Boolean value that enables the creation of a public subnet."
+	default = true
+}
+
+variable "starting_bit_public" {
+	description = "Integer value to set the starting bit range of the generated public subnets."
+	default = 1
+}
+
+variable "natEnabled" {
+	description = "Boolean value that sets up network address translation in the public subnet."
+	default = false
+}
+
+variable "private" {
+	description = "Boolean value that enables the creation of a private subnet."
+	default = false
+}
+
+variable "nat_id" {
+	type = "string"
+	description = "The ID of the NAT gateway for private subnets. (Required if public = false)"
+	default = "none"
+}
+
+variable "starting_bit_private" {
+	description = "Integer value to set the starting bit range of the generated private subnets."
+	default = 101
+}

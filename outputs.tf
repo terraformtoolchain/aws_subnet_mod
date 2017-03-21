798d4ec35f9b35b67f92ed1680381d744797f900
Atlas Kerr <atlaskerr@gmail.com>
2017-03-21 16:47:07 -0500

    added outputs for subnet ids

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "nat_ids" {
 	value = "${ join(",", aws_nat_gateway.nat.*.id) }"
 }
+
+output "public_subnet_ids" {
+	value = "${ join(",", aws_subnet.public.*.id) }"
+}
+
+output "private_subnet_ids" {
+	value = "${ join(",", aws_subnet.private.*.id) }"
+}
